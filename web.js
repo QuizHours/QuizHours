32305b489dafde35fc5a0d3a59f90cbfc586d2ec
Gene Lewis <glewis17@cs.stanford.edu>
2014-07-17 01:25:15 -0700

    Basic course data file-serving implemented server end; client app makes a request for this test data

--- web.js
+++ web.js
@@ -2,18 +2,35 @@
 //https://github.com/heroku-examples/node-ws-test/blob/master/server.js
 
 var WebSocketServer = require('ws').Server,
+  fs = require('fs'),
 	http = require('http'),
+	path = require('path'),
 	express = require('express'),
 	app = express(),
-	port = process.env.PORT || 5000;
-		
+	port = process.env.PORT || 5000,
+  router = express.Router();
+
+router.route('/courses/:filename')
+
+  .get(function(req, res){
+    fs.readFile(path.join(process.cwd(),'/data/courses/'+req.params.filename), 'utf8', function(err, data){
+      if (err){
+        res.send(err);
+      } else {
+        res.json(JSON.parse(data)); //JSON.parse needed to convert string to JSON object
+      }
+    });
+  });
+
+app.use('/data', router);
+
 app.use(express.static(__dirname + '/public'));
 
-//WebSocket stuff...not needed in 1.0
-var server = http.createServer(app);	//Why is this line needed?
+var server = http.createServer(app);
 server.listen(port);
-		
-var wss = new WebSocketServer({server: server});
+
+//WebSocket stuff...not needed in 1.0
+/*var wss = new WebSocketServer({server: server});
 wss.on('connection', function(ws){
 		var id = setInterval(function(){
 				ws.send(JSON.stringify(new Date()), function(){});
@@ -22,4 +39,4 @@ wss.on('connection', function(ws){
 		ws.on('close', function(){
 				clearInterval(id);
 		});
-});
+});*/

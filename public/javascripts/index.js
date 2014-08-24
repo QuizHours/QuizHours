313bd1e7f22bd8ee8c03bd31f1968a9891468305
Gene Lewis <glewis17@cs.stanford.edu>
2014-08-23 20:58:22 -0700

    classcode input added to landing page

---
+++ public/javascripts/index.js
@@ -0,0 +1,15 @@
+(function(){
+    $('#portal').click(function(e){
+        e.preventDefault();
+        $(this).parent().prepend("<form><label>Classcode: " +
+          "<input type='text' id='classcode' /></label>" +
+          "&nbsp;<input id='quizsubmit' type='submit' value='Go To Quiz' /></form>");
+        $('#quizsubmit').click(function(event){
+            event.preventDefault();
+            var classcode = $('#classcode').val();
+            // TODO add an ajax check for validity here
+            location.href = '/quiz?classcode='+classcode;
+        });
+        $(this).hide();
+    });
+})();
